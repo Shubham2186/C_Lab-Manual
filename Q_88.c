d03a44b5144d5b2bffd5455d2777127f4ada14fe
Shubham2186 <inevitable218@gmail.com>
2025-11-30 11:57:33 +0530

    Program of finding factorial of a number using function(recursion))

---
+++ Q_88.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int factorial(int n) {
+    if(n ==0 || n == 1) return 1;
+    return n*factorial(n-1);
+} 
+int main() {
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    printf("Factorial of %d is : %d\n", n, factorial(n));
+    return 0;
+}
