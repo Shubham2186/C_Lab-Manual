cfff58612cbce6942dd83155cf8ac029a7ba473f
Shubham2186 <inevitable218@gmail.com>
2025-12-06 12:52:08 +0530

    Program of swapping two numbers without using a third variable

---
+++ Q_10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+    printf("Enter the value of first number : ");
+    scanf("%d",&num1);
+    printf("Enter the value of second number : ");
+    scanf("%d",&num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    printf("After swapping, first number is %d and second number is %d.\n", num1, num2);
+    return 0;
+}
