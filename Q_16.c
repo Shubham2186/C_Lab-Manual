f1d614e88cb9018d9b516fc007502019a3804467
Shubham2186 <inevitable218@gmail.com>
2025-12-06 14:53:18 +0530

    Program of printing day by its number

---
+++ Q_16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+    printf("Enter number : ");
+    scanf("%d",&day);
+    switch (day) {
+        case 1 :
+            printf("Monday\n");
+            break;
+        case 2 :
+            printf("Monday\n");
+            break;
+        case 3 :
+            printf("Monday\n");
+            break;
+        case 4 :
+            printf("Monday\n");
+            break;
+        case 5 :
+            printf("Monday\n");
+            break;
+        case 6 :
+            printf("Monday\n");
+            break;
+        case 7 :
+            printf("Monday\n");
+            break;
+        default :
+            printf("Invalid input\n");
+            break;
+    }
+    return 0;
+}
