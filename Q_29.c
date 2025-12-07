0471a3448bf8818eb1e84624acd58b42ebbb94b5
Shubham2186 <inevitable218@gmail.com>
2025-12-07 17:23:21 +0530

    Program of finding sum of even numbers from 1 to 100

---
+++ Q_29.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i,sum = 0;
+    for (i = 1;i<=100;i++) {
+        if (i %2 == 0) sum += i;
+    }
+    printf("The sum of all even numbers between 1 to 100 is %d.\n",sum);
+    return 0;
+}
