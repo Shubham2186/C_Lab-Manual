970b74b67383280ac4511ba261044d1ce09bfd5f
Shubham2186 <inevitable218@gmail.com>
2025-12-07 17:12:39 +0530

    Program of printing hello world n times

---
+++ Q_26.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0, n;
+    printf("Enter any number : ");
+    scanf("%d",&n);
+    while(i<n) {
+        printf("Hello World!\n");
+        i++;
+    }
+    return 0;
+}
