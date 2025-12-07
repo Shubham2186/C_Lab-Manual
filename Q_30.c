b695161dbdd389cee2f26935ae3586a61144035c
Shubham2186 <inevitable218@gmail.com>
2025-12-07 17:26:30 +0530

    Program of finding sum of all odd numbers from 1 to n

---
+++ Q_30.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0,i;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    for (i = 1;i<=n;i++) {
+        if (i %2 != 0) sum += i;
+    }
+    printf("The sum of odd numbers from 1 to %d is %d.\n",n, sum);
+    return 0;
+}
